80109a7f6f2e0e5a39c84bae64d289cc1f247c2b
Luke Hoang <hoangminhmung@gmail.com>
2019-12-09 13:10:41 -0500

    add resources

---
+++ public/js/script.js
@@ -0,0 +1,222 @@
+$(document).ready(function(){
+    $(".know_more_mobile").click(function(){
+        $(".main .profile").toggleClass("animated");
+        $(".main .copy").toggleClass("animated");
+        $(".main .headline").toggleClass("animated");
+        $(".know_more_mobile").css("display", "none");   
+        $(".arrow_know_more").css("display", "inline-block");            
+        return false;
+    });
+
+
+    $(".nav_icon>i").click(function(){
+        $(".navigation_mobile .nav").toggleClass("animated"); 
+        $(".fa-bars").toggleClass("hidden"); 
+        $(".fa-times").toggleClass("hidden"); 
+    });
+
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+    });
+    } // End if
+    });
+
+
+
+
+    /* Back to top */
+    // hide #back-top first
+    $(".back_to_top").hide();
+
+    // fade in #back-top
+    $(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back_to_top').fadeIn();
+        } else {
+            $('.back_to_top').fadeOut();
+        }
+    });
+
+    // scroll body to 0px on click
+    $('.back_to_top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+    });
+
+
+    // Add active class 
+    var url = window.location.href;
+    
+    if(url.indexOf('contact') > 0){
+        $('.nav_contact').addClass('active');
+    }
+    else if(url.indexOf('portfolio') > 0){
+        $('.nav_portfolio').addClass('active');
+    }
+    else if(url.indexOf('bamboocopter') > 0){
+        $('.nav_gallery').addClass('active');
+    }
+    else if(url.indexOf('travel') > 0){
+        $('.nav_travel').addClass('active');
+    }
+    else if(url.indexOf('about') > 0){
+        $('.nav_about').addClass('active');
+    }
+    else {
+        $('.nav_home').addClass('active');
+    }
+
+
+
+    $(function() {
+        $(".bio")
+            .mouseover(function() { 
+                $(".profile").css("opacity", 0);
+                $(".info").css("opacity", 1);
+                $(".skills_box").css("opacity", 0);
+                $(".experiences_box").css("opacity", 0);
+            })
+            .mouseout(function() {
+                $(".profile").css("opacity", 1);
+                $(".info").css("opacity", 0);
+                $(".skills_box").css("opacity", 0);
+                $(".experiences_box").css("opacity", 0);
+            });
+    });
+
+    $(function() {
+        $(".skills")
+            .mouseover(function() { 
+                $(".profile").css("opacity", 0);
+                $(".info").css("opacity", 0);
+                $(".skills_box").css("opacity", 1);
+                $(".experiences_box").css("opacity", 0);
+            })
+            .mouseout(function() {
+                $(".profile").css("opacity", 1);
+                $(".info").css("opacity", 0);
+                $(".skills_box").css("opacity", 0);
+                $(".experiences_box").css("opacity", 0);
+            });
+    });
+
+    $(function() {
+        $(".experiences")
+            .mouseover(function() { 
+                $(".profile").css("opacity", 0);
+                $(".info").css("opacity", 0);
+                $(".skills_box").css("opacity", 0);
+                $(".experiences_box").css("opacity", 1);
+            })
+            .mouseout(function() {
+                $(".profile").css("opacity", 1);
+                $(".info").css("opacity", 0);
+                $(".skills_box").css("opacity", 0);
+                $(".experiences_box").css("opacity", 0);
+            });
+    });
+
+    $(function() {
+        $(".h1_title_1")
+            .mouseover(function() { 
+                $(".box_1").css("opacity", 1);
+                $(".box_2").css("opacity", 0);
+                $(".box_3").css("opacity", 0);
+                $(".box_4").css("opacity", 0);
+            }).mouseout(function() {
+                $(".box_1").css("opacity", 1);
+                $(".box_2").css("opacity", 0);
+                $(".box_3").css("opacity", 0);
+                $(".box_4").css("opacity", 0);
+            });
+    });
+
+    $(function() {
+        $(".h1_title_2")
+            .mouseover(function() { 
+                $(".box_1").css("opacity", 0);
+                $(".box_2").css("opacity", 1);
+                $(".box_3").css("opacity", 0);
+                $(".box_4").css("opacity", 0);
+                $(".box_5").css("opacity", 0);
+            }).mouseout(function() {
+                $(".box_1").css("opacity", 0);
+                $(".box_2").css("opacity", 1);
+                $(".box_3").css("opacity", 0);
+                $(".box_4").css("opacity", 0);
+                $(".box_5").css("opacity", 0);
+            });
+    });
+
+    $(function() {
+        $(".h1_title_3")
+            .mouseover(function() { 
+                $(".box_1").css("opacity", 0);
+                $(".box_2").css("opacity", 0);
+                $(".box_3").css("opacity", 1);
+                $(".box_4").css("opacity", 0);
+                $(".box_5").css("opacity", 0);
+            }).mouseout(function() {
+                $(".box_1").css("opacity", 0);
+                $(".box_2").css("opacity", 0);
+                $(".box_3").css("opacity", 1);
+                $(".box_4").css("opacity", 0);
+                $(".box_5").css("opacity", 0);
+            });
+    });
+    $(function() {
+        $(".h1_title_4")
+            .mouseover(function() { 
+                $(".box_1").css("opacity", 0);
+                $(".box_2").css("opacity", 0);
+                $(".box_3").css("opacity", 0);
+                $(".box_4").css("opacity", 1);
+                $(".box_5").css("opacity", 0);
+            }).mouseout(function() {
+                $(".box_1").css("opacity", 0);
+                $(".box_2").css("opacity", 0);
+                $(".box_3").css("opacity", 0);
+                $(".box_4").css("opacity", 1);
+                $(".box_5").css("opacity", 0);
+            });
+    });
+    $(function() {
+        $(".h1_title_5")
+            .mouseover(function() { 
+                $(".box_1").css("opacity", 0);
+                $(".box_2").css("opacity", 0);
+                $(".box_3").css("opacity", 0);
+                $(".box_4").css("opacity", 0);
+                $(".box_5").css("opacity", 1);
+            }).mouseout(function() {
+                $(".box_1").css("opacity", 0);
+                $(".box_2").css("opacity", 0);
+                $(".box_3").css("opacity", 0);
+                $(".box_4").css("opacity", 0);
+                $(".box_5").css("opacity", 1);
+            });
+    });
+
+});
