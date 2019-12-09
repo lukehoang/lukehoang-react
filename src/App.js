d2cd1f020b946a7fb3fc089c96566651df98149e
Luke Hoang <hoangminhmung@gmail.com>
2019-12-09 13:23:54 -0500

    add css, js references

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="navigation">
+          <div class="wrapper flex">
+              <div class="brand">
+                  <h1><a href="https://www.lukehoang.com">Luke Hoang</a></h1>
+              </div>
+              <div class="nav">
+                  <ul class="navbar">
+                      <li><a href="https://www.lukehoang.com" class="nav_home">Home</a></li>
+                      <li><a href="portfolio.php" class="nav_portfolio">Portfolio</a></li>
+                      <li><a href="contact.php" class="nav_contact">Contact</a></li>
+                  </ul>
+                  <ul class="social">
+                      <li><a href="https://www.facebook.com/bamboocopterfilm/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                      <li><a href="https://www.instagram.com/lukemhoang/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                      <li><a href="https://github.com/lukehoang" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                      <li><a href="https://www.linkedin.com/in/lukehoang/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                      </a></li>
+                  </ul>
+              </div>
+          </div>
       </div>
     );
   }
