4f9d1c2909e6a0c04787397bee3865f1a311c999
Luke Hoang <hoangminhmung@gmail.com>
2020-02-05 23:31:31 -0500

    rewrite bio

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -80,7 +80,7 @@ export default class HomePage extends Component {
                             <div className={style.copy}>
                                 <p>Passionate web developer and photographer based in Philadelphia, PA. I learned how to create my first website <a href="https://bamboocopter.net" target="_blank">Bamboocopter.net</a> many years ago because I couldn't afford to pay someone to do it for me. My journey to become a coder has started since then.</p>
                                 <p>I like to keep everything simple. My goals are focus on content and conveying the message that what you want to send to your audiences.</p>
-                                <h2><em>"Choose a job you love, and you will never have to work a day in your life."</em></h2>
+                                <p><em>"Choose a job you love, and you will never have to work a day in your life."</em></p>
                             </div>
                         </div>
                     </div>
