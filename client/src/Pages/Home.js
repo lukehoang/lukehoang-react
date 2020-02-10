70c1353fd79a300b743b789e1dc561f7ca1f1bd4
Luke Hoang <hoangminhmung@gmail.com>
2020-02-09 21:41:58 -0500

    redesign homepage

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -59,10 +59,10 @@ export default class HomePage extends Component {
             <div className={style.wrapper}>
                 <div className={`${style.content} ${style.flex}`}>
                     <div className={`${style.header_nav} ${style.keyframes_slidein}`}>
-                        <h1> <a className={`${style.bio} ${style.h1_desktop} ` + (this.state.info_opacity ? `${style.active}` : '' )} onClick={() => this.handleHoverIn('bio')}>BIO</a></h1>
-                        <h1> <a className={`${style.skills} ${style.h1_desktop} ` + (this.state.skills_opacity ? `${style.active}` : '' )} onClick={() => this.handleHoverIn('skills')}>SKILLS</a></h1>
-                        <h1> <a className={`${style.experiences} ${style.h1_desktop} ` + (this.state.exp_opacity ? `${style.active}` : '' )} onClick={() => this.handleHoverIn('exp')}>EXPERIENCES</a></h1>
-                        <h1><NavLink to="/projects" className={`${style.h1_desktop}`}>WORKS</NavLink></h1>
+                        <h1> <a className={`${style.bio} ${style.h1_desktop} ` + (this.state.info_opacity ? `${style.active}` : '' )} onClick={() => this.handleHoverIn('bio')}>About</a></h1>
+                        <h1> <a className={`${style.experiences} ${style.h1_desktop} ` + (this.state.exp_opacity ? `${style.active}` : '' )} onClick={() => this.handleHoverIn('exp')}>Experiences</a></h1>
+                        <h1> <a className={`${style.skills} ${style.h1_desktop} ` + (this.state.skills_opacity ? `${style.active}` : '' )} onClick={() => this.handleHoverIn('skills')}>Skills</a></h1>
+                        <h1><NavLink to="/projects" className={`${style.h1_desktop}`}>Works</NavLink></h1>
                     </div>
                     <div className={style.profile_mobile}>
                         <img src={profile}/>
@@ -73,6 +73,7 @@ export default class HomePage extends Component {
                     </div>
                     <div className={`${style.box} ${style.info}`} id={style.info} style={{zIndex: this.state.info_opacity ? 9 : 0, opacity: this.state.info_opacity ? 1 : 0}}>
                         <div>
+                            <div className={style.info_img}><img src={profile}/></div>
                             <div className={style.title}>
                                 <h3 className={style.headline}>
                                     Full-Stack <br/>
@@ -80,62 +81,13 @@ export default class HomePage extends Component {
                                 </h3>
                             </div>
                             <div className={style.copy}>
-                                <p>Passionate web developer and photographer based in Philadelphia, PA. I learned how to create my first website <a className={style.color_link} href="https://bamboocopter.net" target="_blank">Bamboocopter.net</a> many years ago because I couldn't afford to pay someone to do it for me. My journey to become a coder has started since then.</p>
-                                <p>I like to keep everything simple. My goals are focus on content and conveying the message that what you want to send to your audiences.</p>
-                                <p><strong>"Choose a job you love, and you will never have to work a day in your life."</strong></p>
+                                <p>Hi! My name is Luke Hoang, a passionate web developer and photographer based in Philadelphia, PA. Many years ago, I learned coding by creating my photography website <a className={style.color_link} href="https://bamboocopter.net" target="_blank">Bamboocopter.net</a>, because I couldn't afford to pay someone to do it <i className="fa fa-smile-o fa-lg" aria-hidden="true"></i>. My journey to become a coder has started since then.</p>
+                                <p>I like to keep things simple and focus on conveying the message that what you want to send to your audiences.</p>
                                 <NavLink className={`${style.btn} ${style.btn_sm}`} to="/contact">Let's talk</NavLink>
+                                <p><strong>"Choose a job you love, and you will never have to work a day in your life."</strong></p>
                             </div>
                         </div>
                     </div>
-
-
-
-                    <div className={`${style.box} ${style.skills_box}`} id={style.skills} style={{zIndex: this.state.skills_opacity ? 9 : 0, opacity: this.state.skills_opacity ? 1 : 0}}>
-                        <div>
-                            <h3>This page was built using React.js</h3>
-                            <div className={style.skill}>
-                                <h3>Front-End</h3>
-                                <p>Javascript/jQuery/React.Js/Bootstrap</p>
-                                <div className={`${style.bar} ${style.front_end}`}>
-                                    <div className={style.black}></div>
-                                    <div className={style.white}></div>
-                                </div>
-                            </div>
-
-
-                            <div className={style.skill}>
-                                <h3>Back-End</h3>
-                                <p>PHP/Node/C#/REST API</p>
-                                <div className={`${style.bar} ${style.back_end}`}>
-                                    <div className={style.black}></div>
-                                    <div className={style.white}></div>
-                                </div>
-                            </div>
-
-
-                            <div className={style.skill}>
-                                <h3>Database</h3>
-                                <p>MySQL/MongoDB</p>
-                                <div className={`${style.bar} ${style.database}`}>
-                                    <div className={style.black}></div>
-                                    <div className={style.white}></div>
-                                </div>
-                            </div>
-
-                            <div className={style.skill}>
-                                <h3>WordPress</h3>
-                                <p>CMS/gutenberg/eCommerce</p>
-                                <div className={`${style.bar} ${style.wordpress}`}>
-                                    <div className={style.black}></div>
-                                    <div className={style.white}></div>
-                                </div>
-                            </div>
-                            <div className={style.skill}>
-                                <NavLink className={`${style.btn} ${style.btn_sm}`} to="/contact">See my works</NavLink>
-                            </div>
-                        </div>
-                    </div>
-
 
 
                     <div className={`${style.box} ${style.experiences_box}`} id={style.experiences} style={{zIndex: this.state.exp_opacity ? 9 : 0, opacity: this.state.exp_opacity ? 1 : 0}}>
@@ -195,6 +147,57 @@ export default class HomePage extends Component {
                             </div>
                         </div>
                     </div>
+
+
+
+                    <div className={`${style.box} ${style.skills_box}`} id={style.skills} style={{zIndex: this.state.skills_opacity ? 9 : 0, opacity: this.state.skills_opacity ? 1 : 0}}>
+                        <div>
+                            <h3>This page was built using React.js</h3>
+                            <div className={style.skill}>
+                                <h3>Front-End</h3>
+                                <p>Javascript/jQuery/React.Js/Bootstrap</p>
+                                <div className={`${style.bar} ${style.front_end}`}>
+                                    <div className={style.black}></div>
+                                    <div className={style.white}></div>
+                                </div>
+                            </div>
+
+
+                            <div className={style.skill}>
+                                <h3>Back-End</h3>
+                                <p>PHP/Node/C#/REST API</p>
+                                <div className={`${style.bar} ${style.back_end}`}>
+                                    <div className={style.black}></div>
+                                    <div className={style.white}></div>
+                                </div>
+                            </div>
+
+
+                            <div className={style.skill}>
+                                <h3>Database</h3>
+                                <p>MySQL/MongoDB</p>
+                                <div className={`${style.bar} ${style.database}`}>
+                                    <div className={style.black}></div>
+                                    <div className={style.white}></div>
+                                </div>
+                            </div>
+
+                            <div className={style.skill}>
+                                <h3>WordPress</h3>
+                                <p>CMS/gutenberg/eCommerce</p>
+                                <div className={`${style.bar} ${style.wordpress}`}>
+                                    <div className={style.black}></div>
+                                    <div className={style.white}></div>
+                                </div>
+                            </div>
+                            <div className={style.skill}>
+                                <NavLink className={`${style.btn} ${style.btn_sm}`} to="/contact">See my works</NavLink>
+                            </div>
+                        </div>
+                    </div>
+
+
+
                 </div>
             </div>
         </div>
